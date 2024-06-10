2e1691d3f856333979e856883658961eb916108b
ftyszyx <whyzi@qq.com>
2024-06-10 22:37:54 +0800

    init

--- src/main/api/index.api.ts
+++ src/main/api/index.api.ts
@@ -1,7 +1,19 @@
 import { BrowserWindow } from 'electron'
 import AppModel from '../model/app.model'
-import { log } from 'util'
 
+function getToken(loginwin: BrowserWindow) {
+  loginwin.webContents.session.cookies.get({ domain: 'bugly.qq.com' }).then((cookies) => {
+    const bugly_session_mid = cookies.find((cookie) => cookie.name == 'bugly-session')
+    if (bugly_session_mid) {
+      console.log('bugly_session', bugly_session_mid?.value)
+      AppModel.getInstance().mainWindow?.webContents.send('bugly-session', bugly_session_mid?.value)
+      loginwin.close()
+      loginwin.webContents.session.clearStorageData().then(() => {
+        console.log('clear storage data')
+      })
+    }
+  })
+}
 export function openBuglyLogin() {
   const loginwin = new BrowserWindow({
     width: 860,
@@ -18,28 +30,8 @@ export function openBuglyLogin() {
   loginwin.webContents.on('will-navigate', (_, url) => {
     console.log('navigate to ', url)
   })
-  loginwin.webContents.on('will-redirect', (_, url) => {
-    console.log('redirect to ', url)
-  })
   loginwin.webContents.on('did-finish-load', async () => {
-    console.log('did-finish-load')
-    const code = 'document.getElementsByTagName("tbody")'
-    const res = await loginwin.webContents.executeJavaScript(code)
-    console.log('res', res)
+    getToken(loginwin)
   })
-  // loginwin.webContents.session.cookies.get({ domain: 'bugly.qq.com' }).then((cookies) => {
-  //   console.log('cookies', cookies)
-  // })
-  // loginwin.webContents.session.cookies.on('changed', (_, cookie, cause, removed) => {
-  //   if (cookie.domain == 'bugly.qq.com' && removed == false) {
-  //     console.log('cookie changed', cookie, cause, removed)
-  //     if (cookie.name == 'bugly_session') {
-  //       AppModel.getInstance().mainWindow?.webContents.send('bugly_session', cookie.value)
-  //     }
-  //     if (cookie.name == 'bugly-session') {
-  //       AppModel.getInstance().mainWindow?.webContents.send('bugly-session', cookie.value)
-  //     }
-  //   }
-  // })
   loginwin.loadURL('https://bugly.qq.com/v2/workbench/apps').then()
 }
